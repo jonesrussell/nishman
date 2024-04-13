e2dd6f2c1f2a332e32873650d505f9acb9dcf7de
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-04-13 00:47:39 +0000

    Initial nishman

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { Play } from './Play';
+import Phaser from 'phaser';
+
+const config = {
+    title: 'Nishman Game',
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    backgroundColor: '#192a56',
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: Play
+};
+
+const game = new Phaser.Game(config);
