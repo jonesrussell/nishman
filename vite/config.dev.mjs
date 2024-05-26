d191d03fdc634fb7b9b5f4ee5e53a9c6d409b13d
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-05-26 17:00:33 +0000

    WIP

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     base: './',
@@ -11,6 +12,7 @@ export default defineConfig({
             }
         },
     },
+    plugins: ['tsconfigPaths'],
     server: {
         port: 8080
     }
